910acfc95323c025f60563aa6cd74ef6ccf7af76
lozik <jarrellporter@gmail.com>
2024-04-07 22:29:36 -0500

    consolidate to main

--- webgui/main.py
+++ webgui/main.py
@@ -1,11 +1,21 @@
+
+import yaml
+import os
 from flask import *
+os.path.dirname(".")
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+with open(os.path.join(parent_dir,"config.yml")) as f:
+    config = yaml.safe_load(f)
 
 app = Flask(__name__)
 
 @app.route('/')
 def url_categories():
+    template = "main.html"
+    if config["use_dynamic_js"]:
+        template = "dynamic.html"
     return render_template(
-        "main.html"
+        template
         )
 
 if __name__ == '__main__':
